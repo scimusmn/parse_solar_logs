a0772627f9a0d3a5498e41cd93410c8b104f3337
bryan kennedy <bkennedy@smm.org>
2013-01-23 17:58:45 -0600

    Implementing external logful reading
    Added argparse so that a logfile can be passed to the sciprt.
    Code now reads through that file and breaks it into a list

--- parse.py
+++ parse.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 from pyparsing import Combine, Literal, Optional, Suppress, Word, nums
+import argparse
 
 
 def main():
-    tests = '''
-    m[mi++]="06.01.13 12:05:00|2489;2602;7093;52;10|2309;2407;5625;52;11|893;33;-2;0.8;2726"
-    m[mi++]="06.01.13 12:00:00|2502;2617;6881;52;10|2323;2418;5428;52;11|897;33;-3;0.0;2653"
-    m[mi++]="06.01.13 11:55:00|2329;2433;6672;52;10|2148;2254;5233;52;11|836;33;-2;0.0;2581"
-    m[mi++]="06.01.13 11:50:00|2501;2620;6474;52;9|2326;2418;5050;52;10|908;33;-3;0.5;2514"
-    m[mi++]="06.01.13 11:45:00|2468;2571;6261;52;9|2262;2350;4853;52;10|892;30;-3;0.4;2440"'''.splitlines()
+    parser = argparse.ArgumentParser(description='Parse data from the Solar\
+                                     Log JS log files')
+    parser.add_argument('logfile', help='A single Solar Log log file')
+    args = parser.parse_args()
+    logfile = args.logfile
 
-    tests.remove('')
+    # Parse logfile into a list
+    with open(logfile) as f:
+        logs = f.readlines()
 
-    print tests
-
-    point = Literal(".")
-    #num = Optional(point + Word(nums))
+    # Debug
+    print logs
 
     # Only whole number strings
     num = Word(nums)
     # Float number - +6.7 or -89.678 or 5 or 892892
+    point = Literal(".")
     fnum = Combine(Word("+-" + nums, nums) +
                    Optional(point + Optional(Word(nums))))
 
@@ -36,8 +37,8 @@ def main():
     interval = (start_trash + quote + date + time + delimiter + inv1 +
                 delimiter + inv2 + delimiter + env + quote)
 
-    for test in tests:
-        stats = interval.parseString(test)
+    for log in logs:
+        stats = interval.parseString(log)
         print stats.asList()
 
 
